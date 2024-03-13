bf972a432266328a81498f8ea15da4ea4d40c87f
tilekl01 <zylkycievtilek@gmail.com>
2024-03-13 09:12:54 +0000

    deploy

--- back_site/settings.py
+++ back_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jbjbj_okaunjn4sn&9i=)mfhnt9d^u3dof=zaocdwd_gk7v4at
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,7 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
